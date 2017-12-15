e3daa6b4cb5e7e7d6b06946eff945d63a0c2e37c
Pablo Edgar <33327985+pab10edgar@users.noreply.github.com>
2017-12-15 10:37:22 -0700

    Update StringChain.java

--- Markov/src/StringChain.java
+++ Markov/src/StringChain.java
@@ -1,8 +1,9 @@
-
 /*
  * @author Pablo Edgar
  * 11/6/2017
+ *
  * Lab #7 - Markov Chain Text Generator
+ *
  * A program that creates a Markov chain from a text file (or files) 
  * and generates some gibberish in a similar style
  */
@@ -23,20 +24,23 @@ public class StringChain {
     private static final String NONWORD = " ";
     private final int order;
 
+     /**
+     * String Chain Constructor
+     * 
+     * @param order of markov chain
+     */
+    public StringChain(int order) {
+        this.order = order;
+    }
+    
+     /**
+     * Nested Suffix Class of Markov Chain
+     */
     public class Suffix {
 
+        // Initialize LinkedList
         private List<String> suf1;
-
-        /**
-         * Suffix Constructor
-         * 
-         * @param str
-         */
-        public Suffix(String str) {
-            suf1 = new LinkedList<>();
-            suf1.add(str);
-        }
-
+        
         /**
          * Default constructor
          */
@@ -44,6 +48,17 @@ public class StringChain {
             suf1 = new LinkedList<>();
         }
 
+        /**
+         * Suffix Constructor
+         * 
+         * @param str
+         */
+        public Suffix(String str) {
+            // Create a new LinkedList to store values
+            suf1 = new LinkedList<>();
+            suf1.add(str);
+        }
+        
         /**
          * Add method to add string to LinkedList
          * 
@@ -54,7 +69,7 @@ public class StringChain {
         }
 
         /**
-         * Get method to get random string
+         * Get method to return random string based on LinkedList
          * 
          * @param randr
          * @return item at 'i' in LinkedList
@@ -64,18 +79,20 @@ public class StringChain {
             return suf1.get(i);
         }
 
+        // Return generic hashCode of LinkedList multiplied by 31 (prime odd number)
+        // for consistency
         @Override
         public int hashCode() {
             return suf1.hashCode() * 31;
         }
-
     }
 
     /**
-     * Prefix Class of Markov Chain
+     * Nested Prefix Class of Markov Chain
      */
     public class Prefix {
 
+        // Initialize the list
         private List<String> pre1;
 
         // Constructor that takes in a List of Strings (LinkedList)
@@ -85,6 +102,7 @@ public class StringChain {
             pre1 = new LinkedList<String>(list);
         }
 
+        // Equals method to see if object in parameter matches prefix
         @Override
         public boolean equals(Object prefix) {
             return pre1.equals(((Prefix) prefix).getPrefix());
@@ -93,7 +111,7 @@ public class StringChain {
         /**
          * Get method for current prefix
          * 
-         * @return pre1
+         * @return pre1 List of Prefix
          */
         public List<String> getPrefix() {
             return pre1;
@@ -111,23 +129,15 @@ public class StringChain {
     }
 
     /**
-     * String Chain Constructor
-     * 
-     * @param order
-     */
-    public StringChain(int order) {
-        this.order = order;
-    }
-
-    /**
      * Add a sequence of items to the chain, as given by a string iterator.
      * 
      * @param itemIter
      */
-    
     public void addItems(Iterator<String> itemIter) {
+        
         List<String> intL = new LinkedList<>();
 
+        // Add non-word padding to chain depending on order 
         for (int i = 0; i < order; i++) {
             intL.add(NONWORD);
         }
@@ -137,35 +147,40 @@ public class StringChain {
 
         // Manipulate Chain by putting items to map
         while (itemIter.hasNext()) {
+            
             String s = itemIter.next();
+            
+            // If chain contains prefix key, add next string to chain
             if (chain.containsKey(p1)) {
                 chain.get(p1).Add(s);
             } else {
                 chain.put(p1, new Suffix(s));
-
             }
 
+            // Add string s to top of chain
             intL.add(s);
+            // Remove string at beginning of chain
             intL.remove(0);
+            
             p1 = new Prefix(intL);
 
         }
-        // Repeat
+        
+        // Repeat with NONWORDS
         for (int i = 0; i < order; i++) {
+            
             String s = NONWORD;
+
             if (chain.containsKey(p1)) {
                 chain.get(p1).Add(s);
             } else {
                 chain.put(p1, new Suffix(s));
-
             }
-
+            
             intL.add(s);
             intL.remove(0);
             p1 = new Prefix(intL);
-
         }
-
     }
 
     /**
@@ -178,6 +193,7 @@ public class StringChain {
      */
     public List<String> generate(int n, Random rand) {
 
+        // Make LinkedList to store values
         List<String> generated = new LinkedList<>();
         List<String> temp = new LinkedList<>();
 
@@ -185,18 +201,24 @@ public class StringChain {
         for (int i = 0; i < order; i++) {
             temp.add(NONWORD);
         }
+        
         // Add new strings to LinkedLists
         for (int j = 0; j < n; j++) {
+            
+            // Create a new Prefix, Suffix and String
             Prefix p = new Prefix(temp);
             Suffix s1 = chain.get(p);
             String v1 = s1.getRand(rand);
+            
+            // Add random string to LinkedList
             generated.add(v1);
+            // Add random string to other LinkedList (temporary)
             temp.add(v1);
+            // Remove first item from LinkedList
             temp.remove(0);
         }
 
+        // Return generated list with random strings
         return generated;
-
     }
-
 }
